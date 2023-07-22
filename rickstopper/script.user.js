7629f77c6c9fd5763c1fe3e33ea4a367d26ec99f
FoxDev <91700492+ItsFoxDev@users.noreply.github.com>
2023-07-22 11:42:07 -0400

    ✏️ Renaming (2/5)

--- rickstopper/script.user.js
+++ rickstopper/script.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         RickStopper
-// @namespace    https://bit.ly/itsfoxdev/
+// @namespace    https://itsfoxdev.github.io
 // @version      3.4
 // @description  Blocks rickrolls
 // @author       FoxDev
 // @run-at       document-start
 // @match        https://www.youtube.com/watch?v=dQw4w9WgXcQ
-// @icon         https://raw.githubusercontent.com/ItsFoxDev/Userscripts/main/RickStopper/icon.png
+// @icon         https://raw.githubusercontent.com/ItsFoxDev/userscripts/main/rickstopper/icon.png
 // @grant        none
 // ==/UserScript==
 
@@ -14,5 +14,5 @@
 if (document.location.href.indexOf('?rsaccept=true') === -1){
     console.log('⚠️ Rickroll detected, redirecting...');
     // Redirect to block page //
-    location.replace('https://itsfoxdev.github.io/Userscripts/RickStopper/');
+    location.replace('https://itsfoxdev.github.io/userscripts/rickstopper/');
 }
