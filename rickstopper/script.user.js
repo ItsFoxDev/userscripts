caa22104b0e9bfc7195334570794af3358cba0d3
FoxDev <91700492+ItsFoxDev@users.noreply.github.com>
2023-07-22 12:10:14 -0400

    🐛 New link + bugfixes

--- rickstopper/script.user.js
+++ rickstopper/script.user.js
@@ -5,7 +5,8 @@
 // @description  Blocks rickrolls
 // @author       FoxDev
 // @run-at       document-start
-// @match        https://www.youtube.com/watch?v=dQw4w9WgXcQ
+// @match        https://www.youtube.com/watch?v=dQw4w9WgXcQ*
+// @match        https://www.youtube.com/watch?v=xvFZjo5PgG0*
 // @icon         https://raw.githubusercontent.com/ItsFoxDev/userscripts/main/rickstopper/icon.png
 // @grant        none
 // ==/UserScript==
